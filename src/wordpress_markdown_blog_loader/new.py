50032ddaed9384dd87f9831b805d2000f68db674
Mark van Holsteijn <mark.van.holsteijn@gmail.com>
2022-05-25 08:23:38 +0200

    bumped  to release 0.5.4

--- src/wordpress_markdown_blog_loader/new.py
+++ src/wordpress_markdown_blog_loader/new.py
@@ -52,6 +52,7 @@ def command(title, subtitle, author, image):
     blog.title = title
     blog.subtitle = subtitle
     blog.status = "draft"
+    blog.slug = slug
     blog.og_description = "TODO: add short SEO description here"
     blog.date = (datetime.now().astimezone() + timedelta(days=7)).replace(
         hour=0, minute=0, second=0, microsecond=0
