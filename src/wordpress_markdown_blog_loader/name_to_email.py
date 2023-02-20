7bd1b733dfdc68f024702081045191ede09e1d91
Mark van Holsteijn <mark.van.holsteijn@gmail.com>
2023-02-20 15:44:55 +0100

    fix: add brand and guessed email address

---
+++ src/wordpress_markdown_blog_loader/name_to_email.py
@@ -0,0 +1,15 @@
+def name_to_email(name: str) -> str:
+    """
+    name to email Xebia style.
+
+    >>> name_to_email("Mark van Holsteijn")
+    'mark.vanholsteijn@xebia.com'
+    >>> name_to_email("Jorge Liauw-a-joe")
+    'jorge.liauwajoe@xebia.com'
+    >>> name_to_email("Jan-Justin van Tonder")
+    'janjustin.vantonder@xebia.com'
+    """
+    email_name_exceptions = {}
+    parts = name.replace("-", "").split()
+    email = f'{parts[0]}{"." if len(parts) > 1 else ""}{"".join(parts[1:])}@xebia.com'.lower()
+    return email_name_exceptions.get(email, email)
