000d2e358ce33d1c00bb359778c5874c7015a50f
Mark van Holsteijn <mark.van.holsteijn@gmail.com>
2023-03-07 10:02:36 +0100

    fix: failed rename of parameter

--- src/wordpress_markdown_blog_loader/remove_newlines.py
+++ src/wordpress_markdown_blog_loader/remove_newlines.py
@@ -14,7 +14,7 @@ def remove_newlines_from_paragraphs(html_document: str) -> str:
     >>> remove_newlines_from_paragraphs('<html><body><p>This is a paragraph\\n with some \\n<b>bold</b> and\\n <i>italic</i> text. And\\n it has some\\n <a href="/">links</a> too.\\n</p></body></html>')
     '<html><body><p>This is a paragraph  with some  <b>bold</b> and  <i>italic</i> text. And  it has some  <a href="/">links</a> too. </p></body></html>'
     """
-    doc = BeautifulSoup(document, "html.parser")
+    doc = BeautifulSoup(html_document, "html.parser")
     for paragraph in doc.find_all("p", recursive=True):
         for child in paragraph.children:
             if isinstance(child, NavigableString):
