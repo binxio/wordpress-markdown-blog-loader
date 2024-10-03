c63c4928754506049bc91bd2a6b6a89b42eab70d
Mark van Holsteijn <mark.van.holsteijn@gmail.com>
2024-10-03 15:10:59 +0200

    fix: workaround cloudflare blocking the wordpress loader

--- src/wordpress_markdown_blog_loader/api.py
+++ src/wordpress_markdown_blog_loader/api.py
@@ -269,7 +269,7 @@ class Wordpress(object):
         self._media: List[Medium] = {}
         self.headers = {
             "accept": "application/json",
-            "User-Agent": "Wordpress markdown blog loader - Python",
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.0 Safari/605.1.15",
         }
         self.session = requests.Session()
 
