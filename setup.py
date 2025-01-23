66da962fb65bf0fe7ecd014b5ff6ff45bf0904eb
Mark van Holsteijn <mark.van.holsteijn@gmail.com>
2025-01-23 22:01:05 +0100

    bumped  to release 1.5.2

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="wordpress-markdown-blog-loader",
-    version="1.5.1",
+    version="1.5.2",
     url="https://github.com/binxio/wordpress-markdown-blog-loader",
     license="APL2",
     author="Mark van Holsteijn",
